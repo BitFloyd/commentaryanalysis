e1d1b5fb2d7f4103226f1c60f47dfb2c82766fea
BitFloyd <sby.jcub@gmail.com>
2019-04-03 18:46:18 -0400

    Add Zipf Law Test

--- commentaryanalysis/wordFrequencies.py
+++ commentaryanalysis/wordFrequencies.py
@@ -2,6 +2,10 @@ from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from collections import OrderedDict
 from matplotlib import pylab
+from corpusFromCommentary import getWordsFromCorpus
+import math
+from scipy import stats
+import numpy as np
 
 def remove_stopwords_from_corpus_words(words):
 
@@ -12,7 +16,6 @@ def remove_stopwords_from_corpus_words(words):
     filtered_words = [word for word in words if word not in stop_words]
 
     return filtered_words
-
 
 def get_frequency_distribution_of_words(words):
 
@@ -55,3 +58,32 @@ def plot_normalized_frequency_distribution(n_fqdist, n=20):
     pylab.show()
 
     return True
+
+def plot_zipf_law_on_corpus(corpus):
+
+    words = getWordsFromCorpus(corpus)
+    words = remove_stopwords_from_corpus_words(words)
+    fdist = FreqDist(words)
+    words = fdist.most_common()
+
+    x = [math.log(i[1]) for i in words]
+    y = [math.log(i) for i in range(1, len(x)+1)]
+
+    (m, b) = pylab.polyfit(x, y, 1)
+    yp = pylab.polyval([m, b], x)
+
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+
+    pylab.plot(x, y,'r')
+    pylab.plot(x,yp,'b')
+
+    pylab.ylim([min(y), max(y)])
+    pylab.xlim([min(x), max(x)])
+    pylab.text(x=1,y=1,s = "Best Fit Line (Blue) \nslope = {slope}".format(slope=np.round(slope,2)))
+    pylab.grid(True)
+    pylab.ylabel('Counts of words (log)')
+    pylab.xlabel('Ranks of words (log)')
+    pylab.title('ZIPF LAW TEST ON CORPUS. IDEALLY SLOPE OF THE LINE MUST BE = -1 for IDEAL ZIPF CASE')
+    pylab.show()
+
