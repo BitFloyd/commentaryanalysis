7b50dcbe32db562ab3d9364aeafdb3eb421f16c5
BitFloyd <sby.jcub@gmail.com>
2019-06-14 16:02:50 -0400

    fix handling of empty srt files

--- commentaryanalysis/sentenceEncoder.py
+++ commentaryanalysis/sentenceEncoder.py
@@ -20,9 +20,18 @@ def getUniversalSentenceEncodingsFromCorpus(corpus):
         messages.append(sent)
 
     
+    message_embeddings = getUniversalSentenceEncodingsFromListOfLines(messages)
+
+    return message_embeddings
+
+
+def getUniversalSentenceEncodingsFromListOfLines(list_of_lines):
+
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
+    embed = hub.Module(module_url)
+
     with tf.Session() as session:
         session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-        message_embeddings = session.run(embed(messages))
-
+        message_embeddings = session.run(embed(list_of_lines))
 
     return message_embeddings
